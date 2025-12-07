068487ee15cd3e77baf2273ff8c0f11757de7a21
jonfoong <jonahfoong@gmail.com>
2025-12-07 18:01:05 +0100

    update main.py and move api dependencies from pinecone to zilliz

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 from openai import OpenAI
 from google.cloud import bigquery
-from pinecone import Pinecone
-#from params.api_keys import *
+from params.api_keys import *
+import os
+import google.auth
+from pymilvus import MilvusClient
 
 from summarize_positions import handle_positions_extraction, handle_positions_creation
 from summarize_bills import handle_bill_summaries_extraction, handle_bill_summaries_creation, check_bill_updates
 
 # initialize clients
 gpt_client = OpenAI()
-gbq_client = bigquery.Client()
-pc = Pinecone()
-positions_index = pc.Index("singapore-speeches-positions")
-summaries_index = pc.Index("singapore-bill-summaries")
+
+
+SCOPES = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/drive.readonly",
+]
+creds, project_id = google.auth.default(scopes=SCOPES)
+gbq_client = bigquery.Client(project=project_id, credentials=creds)
+
+zilliz_client = MilvusClient(
+    uri=os.getenv("ZILLIZ_CLIENT_URI"),
+    token=os.getenv("ZILLIZ_API_KEY"), 
+)
 
 def generate_summaries(request):
     """Cloud Function entry point to generate summaries."""
     try:
         # start with speech positions
-        handle_positions_extraction(gbq_client, gpt_client, positions_index)
+        handle_positions_extraction(gbq_client, gpt_client, zilliz_client)
         handle_positions_creation(gbq_client, gpt_client)
 
         # handle summaries for bills
-        handle_bill_summaries_extraction(gbq_client, gpt_client, summaries_index)
+        handle_bill_summaries_extraction(gbq_client, gpt_client, zilliz_client)
         handle_bill_summaries_creation(gbq_client, gpt_client)
 
         # check bill updates
-        check_bill_updates(gbq_client, summaries_index)
+        check_bill_updates(gbq_client, zilliz_client)
         return 'Summaries run successfully.', 200
     except Exception as e:
         print(f"Error running function: {e}")
--- params/api_keys.py
+++ params/api_keys.py
@@ -6,5 +6,8 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "token/gcp_token.json"
 # gpt key
 os.environ["OPENAI_API_KEY"] = open("token/gpt_api_token.txt", 'r').readlines()[0]
 
-# pinecone api key
-os.environ['PINECONE_API_KEY'] = open("token/pinecone_token.txt", 'r').readlines()[0]
+# zilliz api key
+os.environ['ZILLIZ_API_KEY'] = open("token/zilliz_token.txt", 'r').readlines()[0]
+
+# zilliz client uri
+os.environ['ZILLIZ_CLIENT_URI'] = open("token/zilliz_uri.txt", 'r').readlines()[0]
